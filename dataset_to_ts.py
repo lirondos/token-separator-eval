5ac2bcdfbe80fe75d213086582f75b72f9a467c3
Elena Álvarez Mellado <ealvarezmellado@gmail.com>
2023-09-04 16:34:32 +0200

    Add files via upload

---
+++ dataset_to_ts.py
@@ -0,0 +1,34 @@
+from datasets import load_dataset
+
+def to_ts_notation(item):
+    tokens_new = []
+    ner_tags_new = []
+    for token, tag in zip(reversed(tokens), reversed(ner_tags)):
+        if tag==0:
+            ner_tags_new.insert(0, tag)
+            tokens_new.insert(0, token)
+            ner_tags_new.insert(0, tag)
+            tokens_new.insert(" ", token)
+        elif tag%2==0:
+            ner_tags_new.insert(0, tag-1)
+            tokens_new.insert(0, token)
+            ner_tags_new.insert(0, tag-1)
+            tokens_new.insert(" ", token)
+        else:
+            ner_tags_new.insert(0, tag)
+            tokens_new.insert(0, token)
+            ner_tags_new.insert(0, 0)
+            tokens_new.insert(0, " ")
+        
+    item["tokens_ts"] = tokens_new
+    item["ner_tags_ts"] = ner_tags_new
+    return item
+    
+dataset = load_dataset("wnut_17", split="train")
+    
+updated_dataset = dataset.map(to_ts_notation)
+
+for i in dataset[:10]:
+    print(dataset[i])
+
+
