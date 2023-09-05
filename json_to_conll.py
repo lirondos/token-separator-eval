722e753453fd77a3e55d1bc769ca6b934dcaacf9
Elena Álvarez Mellado <ealvarezmellado@gmail.com>
2023-09-05 11:24:47 +0200

    json to conll file

--- json_to_conll.py
+++ json_to_conll.py
@@ -6,20 +6,26 @@ import csv
  
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--json_file", help="path to json", type=str)
-parser.add_argument("--output", help="path to output file", type=str)
+parser.add_argument("--goldstandard", help="path to json", type=str)
+parser.add_argument('--predictions', help="path to predictions.txt", type=str)
+parser.add_argument("--output", help="path where output file will be stored", type=str)
+#parser.add_argument('--is_predictions', action='store_true')
+
                     
 args = parser.parse_args()
 
-json_file = Path(args.json_file)
+goldstandard = Path(args.goldstandard)
 
 with open(args.output, 'w', newline='') as tsvfile:
     writer = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
     
-    with open(json_file) as f:
         for line in f:
-            item = json.loads(line)
-            for token, tag in zip(item["tokens"], item["ner"]):
-                writer.writerow([token, tag])
-            writer.writerow([])
-            writer.writerow([])
+            if args.predictions:
+                predictions = Path(args.predictions)
+                with open(goldstandard) as goldstandard_lines, open(predictions) as predictions_lines:
+                    for g, p in zip(goldstandard_lines, predictions_lines):
+                        g_json = json.loads(g)
+                        for token, predicted_tag in zip(g_json["tokens"], p.split()):
+                            writer.writerow([token, tag])
+                    writer.writerow([])
+                    
