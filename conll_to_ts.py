2be7dc126bb6b7b074072da5f14e46b0ec31a218
Elena Álvarez Mellado <ealvarezmellado@gmail.com>
2023-09-06 10:45:03 +0200

    Minor change

--- conll_to_ts.py
+++ conll_to_ts.py
@@ -37,7 +37,8 @@ with open(input, "r", encoding="utf-8") as f:
                     to_ts.insert(0, (" ", "O"))
 
         else: # line is blank
-            to_ts.pop(0) # we remove the extra empty space
+            if to_ts: # avoid popping the first time bc list is empty
+                to_ts.pop(0) # we remove the extra empty space
             to_ts.insert(0, ())
 
 with open(output, 'w', newline='') as csvfile:
