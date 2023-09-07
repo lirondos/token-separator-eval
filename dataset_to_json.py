e0ae64f5ef429aac5da76221a30ae9d2d16a1eeb
Elena Álvarez Mellado <ealvarezmellado@gmail.com>
2023-09-07 11:39:05 +0200

    Dataset to json files

---
+++ dataset_to_json.py
@@ -0,0 +1,69 @@
+from datasets import load_dataset
+
+id2label = {
+    0: "O",
+    1: "B-corporation",
+    2: "I-corporation",
+    3: "B-creative-work",
+    4: "I-creative-work",
+    5: "B-group",
+    6: "I-group",
+    7: "B-location",
+    8: "I-location",
+    9: "B-person",
+    10: "I-person",
+    11: "B-product",
+    12: "I-product",
+}
+label2id = {
+    "O": 0,
+    "B-corporation": 1,
+    "I-corporation": 2,
+    "B-creative-work": 3,
+    "I-creative-work": 4,
+    "B-group": 5,
+    "I-group": 6,
+    "B-location": 7,
+    "I-location": 8,
+    "B-person": 9,
+    "I-person": 10,
+    "B-product": 11,
+    "I-product": 12,
+}
+
+def to_ts_notation(item):
+    tokens_new = []
+    ner_tags_new = []
+    tokens_reversed = reversed(item["tokens"])
+    ner_tags_reversed = reversed(item["ner_tags"])
+    for token, tag in zip(tokens_reversed, ner_tags_reversed):
+        if tag==0:
+            ner_tags_new.insert(0, id2label[tag])
+            tokens_new.insert(0, token)
+            ner_tags_new.insert(0, id2label[tag])
+        elif tag%2==0:
+            ner_tags_new.insert(0, id2label[tag-1])
+            tokens_new.insert(0, token)
+            ner_tags_new.insert(0, id2label[tag-1])
+        else:
+            ner_tags_new.insert(0, id2label[tag])
+            tokens_new.insert(0, token)
+            ner_tags_new.insert(0, id2label[0])
+        tokens_new.insert(0, "||")
+    tokens_new.pop(0) # we added an extra space at the beginning, we remove it now
+    ner_tags_new.pop(0)
+    item["tokens"] = tokens_new
+    item["ner"] = ner_tags_new
+    del item["ner_tags"]
+    #del item["tokens"]
+    print(ner_tags_new)
+    print(item)
+    return item
+    
+dataset_train = load_dataset("wnut_17", split="train")
+dataset_val = load_dataset("wnut_17", split="validation")
+dataset_test = load_dataset("wnut_17", split="test")
+    
+dataset_train.to_json("wnut_17_bio/train.json")
+dataset_val.to_json("wnut_17_bio/val.json")
+dataset_test.to_json("wnut_17_bio/test.json")
