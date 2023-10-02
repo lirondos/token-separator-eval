fefe4f35a5336029a8f27de985ffc5b4d48f69f3
Elena Álvarez Mellado <ealvarezmellado@gmail.com>
2023-10-02 14:45:36 +0200

    rank

--- rank.py
+++ rank.py
@@ -17,7 +17,7 @@ dev = dict()
 
 files = Path(args.folder).glob('*')
 for file in files:
-    file_name = Path(file).stem()
+    file_name = file.stem
     model, rest = file_name.split('_', 1)
     metric, split = rest.rsplit('_', 1)
     df = pd.read_csv(files, sep=",", header=0)
